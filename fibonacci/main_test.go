7aa3fdfd1608295ab513dd2c104e4937448230b1
Keith Noguchi <keith@noguchi.us>
2023-03-30 15:49:53 -0700

    a unit test to compare with the recursive version

---
+++ fibonacci/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import "testing"
+
+func recursiveFibonacci(n uint64) uint64 {
+	if n < 2 {
+		return n
+	}
+	return recursiveFibonacci(n - 2) + recursiveFibonacci(n - 1)
+}
+
+func TestFibonacci(t *testing.T) {
+	for n := uint64(0); n < 40; n++ {
+		if got, want := fibonacci(n), recursiveFibonacci(n); got != want {
+			t.Fatalf("fibonacci(%d): %d != %d", n, got, want)
+		}
+	}
+}
