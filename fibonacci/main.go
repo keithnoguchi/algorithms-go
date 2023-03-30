7c0e70cea45fc4b3dfb3d0638cde35175ef0690a
Keith Noguchi <keith@noguchi.us>
2023-03-30 15:43:50 -0700

    an iterative fibonacci sequence generator

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -3,6 +3,16 @@ package main
 
 import "fmt"
 
+func fibonacci(n uint64) uint64 {
+	var x, y uint64 = 0, 1
+	for i := uint64(0); i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
+
 func main() {
-	fmt.Println("Hello world")
+	for n := uint64(0); n < 1_000; n++ {
+		fmt.Printf("fibonacci(%d) = %d\n", n, fibonacci(n))
+	}
 }
