e8f47d4dfa2508fe599d6c5553c2fc1a38a2d17d
Keith Noguchi <keith@noguchi.us>
2023-03-30 15:18:29 -0700

    change the local variable name

--- gcd/main.go
+++ gcd/main.go
@@ -11,7 +11,7 @@ func gcd(x, y int) int {
 }
 
 func main() {
-	var tests = []struct{ x, y int }{
+	inputs := []struct{ x, y int }{
 		{0, 0},
 		{12, 9},
 		{12, 6},
@@ -19,8 +19,8 @@ func main() {
 		{10, 20},
 	};
 
-	for _, t := range tests {
-		fmt.Printf("gcd(%2d, %2d) = %2d\n", t.x, t.y, gcd(t.x, t.y))
-		fmt.Printf("gcd(%2d, %2d) = %2d\n", t.y, t.x, gcd(t.y, t.x))
+	for _, arg := range inputs {
+		fmt.Printf("gcd(%2d, %2d) = %2d\n", arg.x, arg.y, gcd(arg.x, arg.y))
+		fmt.Printf("gcd(%2d, %2d) = %2d\n", arg.y, arg.x, gcd(arg.y, arg.x))
 	}
 }
