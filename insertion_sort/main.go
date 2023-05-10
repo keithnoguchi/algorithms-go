d76212b6b28d5ed746c2b9621e766ba883cfd41e
Keith Noguchi <keith@noguchi.us>
2023-05-10 06:21:01 -0700

    An inplace insertion sort
    It's a good recursion exercise, but
    it's O(n^2) in case the input is
    sorted, like the example.

--- insertion_sort/main.go
+++ insertion_sort/main.go
@@ -1,4 +1,4 @@
-// An insertion sort with binary tree
+// An insertion sort.
 package main
 
 import "fmt"
@@ -8,27 +8,41 @@ type tree struct {
 	left, right *tree
 }
 
-func sort(values []int) {
-	var tree *tree
-	for _, v := range values {
-		tree = add(tree, v)
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, value := range values {
+		root = add(root, value)
 	}
-	appendValues(values[:0], tree)
+	appendValue(values[:0], root)
 }
 
-func appendValues(values []int, tree *tree) []int {
+func add(node *tree, value int) *tree {
+	if node == nil {
+		node = new(tree)
+		node.value = value
+	} else if value < node.value {
+		node.left = add(node.left, value)
+	} else {
+		node.right = add(node.right, value)
+	}
+	return node
+}
+
+func appendValue(values []int, node *tree) []int {
+	if node != nil {
+		values = appendValue(values, node.left)
+		values = append(values, node.value)
+		values = appendValue(values, node.right)
+	}
 	return values
-}
-
-func add(tree *tree, value int) *tree {
-	return nil
 }
 
 func main() {
 	var values []int
-	for i := 100; i > 0; i-- {
+	for i := 100_000; i >= 0; i-- {
 		values = append(values, i)
 	}
-	sort(values)
-	fmt.Println(values)
+	Sort(values)
+	fmt.Printf("%q\n", values[len(values)-1])
 }
