df7168b3552ffefb092a5b6fbbf2d23a6234cab1
Keith Noguchi <keith@noguchi.us>
2023-05-10 06:43:31 -0700

    An in-place insertion sort with the binary tree (#7)

---
+++ insertion_sort/main.go
@@ -0,0 +1,48 @@
+// An in-place insertion sort.
+package main
+
+import "fmt"
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, value := range values {
+		root = add(root, value)
+	}
+	appendValue(values[:0], root)
+}
+
+func add(node *tree, value int) *tree {
+	if node == nil {
+		node = new(tree)
+		node.value = value
+	} else if value < node.value {
+		node.left = add(node.left, value)
+	} else {
+		node.right = add(node.right, value)
+	}
+	return node
+}
+
+func appendValue(values []int, node *tree) []int {
+	if node != nil {
+		values = appendValue(values, node.left)
+		values = append(values, node.value)
+		values = appendValue(values, node.right)
+	}
+	return values
+}
+
+func main() {
+	var values []int
+	for i := 10_000; i >= 0; i-- {
+		values = append(values, i)
+	}
+	Sort(values)
+	fmt.Printf("%d\n", values[len(values)-1])
+}
